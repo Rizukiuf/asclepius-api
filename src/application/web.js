7d80e8f3b9e42333991e9722dea42ab69bd3083d
Rizukiuf <rizkiutamafauzi@gmail.com>
2024-12-08 03:18:48 +0700

    add allow cors

--- src/application/web.js
+++ src/application/web.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const { predictRouter } = require('../route/api');
 const { errorMiddleware } = require('../middleware/error-middleware');
 
 const web = express();
+
+const corsOptions = {
+	origin: 'https://submissionmlgc-rizkiutamafauzi.et.r.appspot.com',
+	optionsSuccessStatus: 200
+}
+web.use(cors(corsOptions));
+
 web.use(express.json());
 
 web.use('/predict', predictRouter);
